0a5f173338f463ac6a0cdb54f9921a42fa168c49
Avinay Singh <avinaysingh@Avinays-MacBook-Air.local>
2024-10-16 15:05:28 +0530

    first commit

---
+++ arrays.java
@@ -0,0 +1,12 @@
+ class array_1{
+
+       public static void main (String args[]){
+
+       int[]ar1={5,6,7,8,9,10};
+
+         for (int i=0; i<ar1.length;i++)
+         System.out.println("Index:-  "+i+" Value:- "+ar1[i]);
+
+
+     }
+  }
