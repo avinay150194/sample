e2b0e6331c39340c5365f99d93595d928a16f879
xyz@gmail.com <avinaysingh@Avinays-MacBook-Air.local>
2024-10-16 15:25:44 +0530

    second commit

--- arrays.java
+++ arrays.java
@@ -2,7 +2,7 @@
 
        public static void main (String args[]){
 
-       int[]ar1={5,6,7,8,9,10};
+       int[]ar1={5,6,7,8,9,10};//intger values updated
 
          for (int i=0; i<ar1.length;i++)
          System.out.println("Index:-  "+i+" Value:- "+ar1[i]);
